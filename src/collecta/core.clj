36f4731a72b7a3ea58b408f75f026ee54f1be8d7
Mark Gunnels <markgunnels@gmail.com>
2010-08-29 06:52:39 -0400

    finished breaking everything out. need to better use the connection

--- src/collecta/core.clj
+++ src/collecta/core.clj
@@ -16,44 +16,32 @@
 (def *COLLECTA-SEARCH-URL* "search.collecta.com")
 (declare *collecta-connection*)
 
-(defn- create-connection
-  (new XMPPConnection
-       (new ConnectionConfiguration *COLLECTA-CONNECTION-URL*)))
-
-(defn- establish-connection
-  []
-  (let [conn (create-connection)]
-    (doto conn
-      (.connect)
-      (.loginAnonymously)
-      (.addPacketListener (packet-listener) (packet-filter)))))
-
-(defn packet-filter
+(defn- packet-filter
   []
   (proxy [PacketFilter] [] 
     (accept [packet] true)))
 
-(defn- extract-event-packet
+(defn- extract-event-packet-extension
   [packet]
   (.getExtension packet "event" "http://jabber.org/protocol/pubsub#event"))
 
-(defn packet-to-xml
+(defn- packet-to-xml
   [packet-event]
   (clojure.xml/parse
-   (ByteArrayInputStream. (.getBytes (.toXML pe) "UTF-8"))))
+   (ByteArrayInputStream. (.getBytes (.toXML packet-event) "UTF-8"))))
 
-(defn packet-listener
-  [processing-fn]
+(defn- packet-listener
+  [on-result-fn]
   (proxy [PacketListener] []
     (processPacket
      [packet]
      (let [pe (extract-event-packet-extension packet)]
        (if (not (nil? pe))
-         (apply processing-fn (packet-to-xml pe)))))))
+         (apply on-result-fn (packet-to-xml pe)))))))
 
 ;; todo: Build the xml message with prxml.
 ;;       Allow more than one query term to be submitted.
-(defn iq-search-packet
+(defn- iq-search-packet
   [term jid apikey]
   (proxy [IQ] []
     (getChildElementXML
@@ -74,12 +62,32 @@
            "</x></options>"
            "</pubsub>"))))
 
-(def iq-packet (iq-search-packet "$AAPL" (.getUser conn) "956718aa639f309c0c3d3dd221c9da2d"))
+(defn- create-connection
+  []
+  (new XMPPConnection
+       (new ConnectionConfiguration *COLLECTA-CONNECTION-URL*)))
 
-(doto iq-packet
-  (.setType IQ$Type/SET)
-  (.setFrom (.getUser conn))
-  (.setTo *COLLECTA-SEARCH-URL*))
+(defn- establish-connection
+  [on-result-fn]
+  (let [conn (create-connection)]
+    (doto conn
+      (.connect)
+      (.loginAnonymously)
+      (.addPacketListener (packet-listener on-result-fn) (packet-filter)))
+    conn))
 
-(.sendPacket conn iq-packet)
+(defn- make-iq-packet
+  [api-key term conn]
+  (let [iq-packet (iq-search-packet term (.getUser conn) api-key)]
+    (doto iq-packet
+      (.setType IQ$Type/SET)
+      (.setFrom (.getUser conn))
+      (.setTo *COLLECTA-SEARCH-URL*))))
 
+(defn search-collecta
+  [api-key term on-result-fn]
+  (let [conn (establish-connection on-result-fn)]
+    (.sendPacket conn (make-iq-packet api-key term conn))))
+
+
+;"956718aa639f309c0c3d3dd221c9da2d"
